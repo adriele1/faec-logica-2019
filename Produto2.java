e0634cebbfbcb95f79062eec36bb5a7cbdc11a01
adriele1 <55115452+adriele1@users.noreply.github.com>
2019-09-23 19:24:43 -0300

    produto2

---
+++ Produto2.java
@@ -0,0 +1,36 @@
+public class Produto2 {
+	
+	String nome;
+	int codigo;
+	int preco;
+	String mes;
+
+	public static void main(String[]args){
+
+
+	Produto p1, p2, p3;
+	
+	p1 = new Produto();
+	p1.nome = "Debi e loide";
+	p1.codigo = 10;
+	p1.preco = 50;
+	p1.mes = "agosto";
+	
+	for (int i=0; j<30; p1.vendas[i++] = i^2);
+	
+	int posComMaiorQtd = 0;
+	int maiorQtd = -1;
+	
+	for (int pos=0; pos<30; pos++){
+		if (p1.vendas[pos] > maiorQtd = pos;
+		maiorQtd = p1.vendas[pos];
+	
+	
+
+
+	}
+	}
+
+
+
+}
